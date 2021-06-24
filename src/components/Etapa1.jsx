9bd701a0219fb66f08156ccee40dbd5ab02b7693
Thiago <asm.thiago@gmail.com>
2021-06-24 19:08:56 -0300

    Add padding 10px

--- src/components/Etapa1.jsx
+++ src/components/Etapa1.jsx
@@ -1,5 +1,6 @@
 import TextField from '@material-ui/core/TextField';
 import FormControl from '@material-ui/core/FormControl';
+import useStyles from '../components/style';
 
 export default function Etapa1({
   email,
@@ -10,26 +11,32 @@ export default function Etapa1({
   setConfirm,
   errors,
 }) {
+  const classes = useStyles();
+
   return (
     <div>
       <FormControl>
-        <TextField
-          error={errors.email}
-          onChange={setEmail}
-          label="Email"
-          type="email"
-          variant="outlined"
-          value={email}
-        />
+        <div className={classes.inputs}>
+          <TextField
+            error={errors.email}
+            onChange={setEmail}
+            label="Email"
+            type="email"
+            variant="outlined"
+            value={email}
+          />
+        </div>
+        <div className={classes.inputs}>
         <TextField
           error={errors.password}
           onChange={setPassword}
           label="Senha"
           type="password"
           variant="outlined"
-          
           value={password}
         />
+        </div>
+        <div className={classes.inputs}>
         <TextField
           error={errors.confirm}
           onChange={setConfirm}
@@ -38,6 +45,7 @@ export default function Etapa1({
           variant="outlined"
           value={confirm}
         />
+        </div>
       </FormControl>
     </div>
   );
